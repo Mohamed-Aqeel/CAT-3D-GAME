fee8eae18c53a555003d31db1031fed5e55bdbe9
mohamed aqeel <mohamedakil936@gmail.com>
2023-06-30 17:51:13 +0300

    .

---
+++ ONE SHOT/Assets/Scripts/Gun.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float Damage = 10f;
+    public float Range = 100f;
+
+    public ParticleSystem muzzleFlash;
+    public float FireRate = 15f;
+    public Camera fpsCam;
+
+    private float nextTimeToFire = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / FireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit Hit;
+        if (Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward,out Hit,Range))
+        {
+            Debug.Log(Hit.transform.name);
+            Target target = Hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(Damage);
+            }
+        }
+    }
+
+}
---
+++ ONE SHOT/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController Controller;
+    public float Speed = 12f;
+    public float Gravity = -9.81f;
+    public float JumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public Vector3 Velocity;
+    bool IsGrounded;
+    // Update is called once per frame
+    void Update()
+    {
+        IsGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (IsGrounded && Velocity.y < 0)
+        {
+            Velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        Controller.Move(move * Speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded)
+        {
+            Velocity.y = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+        }
+
+        Velocity.y += Gravity * Time.deltaTime;
+
+        Controller.Move(Velocity * Time.deltaTime);
+    }
+}
